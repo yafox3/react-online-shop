752b99b7ccc8a10186d5b0f53d5abe930e0b3709
Ruslan <ruslan.kret@yandex.ru>
2023-05-05 01:48:12 +0300

    added animation onBuy a product

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,9 +1,10 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useTypedSelector } from '../../hooks/useTypedSelector'
 import { IProduct } from '../../models'
 import { addToCart, updateCartCost } from '../../store/action-creators/cart'
 import { isExist } from '../../utils/utils'
+import './Animation.scss'
 import styles from './Product.module.scss'
 
 interface ProductProps {
@@ -12,6 +13,7 @@ interface ProductProps {
 
 const Product: FC<ProductProps> = ({ product }) => {
 	const {products, totalCost} = useTypedSelector(state => state.cart)
+	const [fly, setFly] = useState(false)
 	const dispatch = useDispatch()
 
 	const onBuy = () => {
@@ -27,46 +29,63 @@ const Product: FC<ProductProps> = ({ product }) => {
 		} else {
 			dispatch(addToCart(product))
 		}
+
+		setFly(true)
+		setTimeout(() => {
+			setFly(false)
+		},1000)
 	}
 
 	return (
-		<div className={styles.product}>
-			<div className={styles.product__img}>
-				<img
-					src={product.img}
-					alt={product.title}
-					draggable='false'
-				/>
+		<>
+			<div className={fly ? 'to-cart-animation' : styles.product_hidden}>
+				<div className={styles.product__img}>
+					<img
+						src={product.img}
+						alt={product.title}
+						draggable='false'
+					/>
+				</div>
 			</div>
 
-			<div className={styles.product__title_price}>
-				<span className={styles.title}>{product.title}</span>
-				<span className={styles.price}>{product.price} ₽</span>
+			<div className={styles.product}>
+				<div className={styles.product__img}>
+					<img
+						src={product.img}
+						alt={product.title}
+						draggable='false'
+					/>
+				</div>
+	
+				<div className={styles.product__title_price}>
+					<span className={styles.title}>{product.title}</span>
+					<span className={styles.price}>{product.price} ₽</span>
+				</div>
+	
+				<div className={styles.product__rate_buy}>
+					<span>
+						<svg
+							width='25'
+							height='23'
+							viewBox='0 0 25 23'
+							fill='none'
+							xmlns='http://www.w3.org/2000/svg'>
+							<path
+								d='M12.6268 18.0143L5.41618 22.3656L7.37647 14.2449L0.960754 8.81491L9.38215 8.14829L12.6268 0.439671L15.8715 8.14829L24.2941 8.81491L17.8771 14.2449L19.8374 22.3656L12.6268 18.0143Z'
+								fill='#FFCE7F'
+							/>
+						</svg>
+						{product.rate}
+					</span>
+					<button 
+						onClick={onBuy}
+						className={styles.btn}
+					>
+						Купить
+					</button>
+				</div>
 			</div>
-
-			<div className={styles.product__rate_buy}>
-				<span>
-					<svg
-						width='25'
-						height='23'
-						viewBox='0 0 25 23'
-						fill='none'
-						xmlns='http://www.w3.org/2000/svg'>
-						<path
-							d='M12.6268 18.0143L5.41618 22.3656L7.37647 14.2449L0.960754 8.81491L9.38215 8.14829L12.6268 0.439671L15.8715 8.14829L24.2941 8.81491L17.8771 14.2449L19.8374 22.3656L12.6268 18.0143Z'
-							fill='#FFCE7F'
-						/>
-					</svg>
-					{product.rate}
-				</span>
-				<button 
-					onClick={onBuy}
-					className={styles.btn}
-				>
-					Купить
-				</button>
-			</div>
-		</div>
+		</>
 	)
 }
 
